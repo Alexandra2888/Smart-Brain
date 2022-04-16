61acb532fbd8b98c265c177cf81c34451058ca4f
Alexandra2888 <moldovan.alexandra28@gmail.com>
2022-04-16 23:17:53 +0300

    updating file

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://intense-plateau-31954.herokuapp.com', {
+    fetch('http://localhost:3000/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
